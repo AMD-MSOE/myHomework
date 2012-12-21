3de4c39333475fef5d4fd54641e062032124a242
Josh Ault <josh.ault3@gmail.com>
2012-12-20 18:11:41 -0600

    added calendar actiivty

---
+++ src/com/amd/myhomework/CalendarActivity.java
@@ -0,0 +1,14 @@
+package com.amd.myhomework;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalendarActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstance) {
+		super.onCreate(savedInstance);
+		setContentView(R.layout.activity_calendar);
+	}
+
+}
