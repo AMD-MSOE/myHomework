f56ed971955ca14beb0e85ef4156087ae1fa8f82
Tom Duex <hippyjonyo@gmail.com>
2013-01-10 18:13:52 -0600

    Changed several things related to the Main Screen
    Changed the Main Screen to go to the Calendar Screen and include a
    Calendar button

--- src/com/amd/myhomework/MainActivity.java
+++ src/com/amd/myhomework/MainActivity.java
@@ -5,24 +5,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 	
-	Button calendarButton;
+	ImageButton calendarButton;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.six_icon_screen);
 		
-		calendarButton = (Button) findViewById(R.id.calendarButton);
+		calendarButton = (ImageButton) findViewById(R.id.calendarButton);
 		calendarButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				
+				goToCalendar();
 			}
 			
 		});
+		
+	}
+	
+	private void goToCalendar() {
+		Intent calendarChange = new Intent(MainActivity.this, CalendarActivity.class);
+		this.startActivity(calendarChange);
 	}
 }
