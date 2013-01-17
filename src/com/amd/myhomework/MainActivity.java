64c2d969f340055db46cb3337ddc85c97bfaa9c2
Tom Duex <hippyjonyo@gmail.com>
2013-01-17 17:52:05 -0600

    fixed an error in the classes layout file

--- src/com/amd/myhomework/MainActivity.java
+++ src/com/amd/myhomework/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
 		calendarButton.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(View v) {
 				switchActivity(SwitchActivity.Calendar);
 			}
 			
