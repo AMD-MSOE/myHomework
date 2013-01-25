17ad87fd28bab5f7450769ea21ac9e9b7f55725d
Josh Ault <josh.ault3@gmail.com>
2013-01-24 18:02:13 -0600

    pushing adapter skeleton

---
+++ src/com/amd/myhomework/adapters/SimpleListAdapter.java
@@ -0,0 +1,57 @@
+package com.amd.myhomework.adapters;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.amd.myhomework.R;
+
+/**
+ * This list adapter can be used for homework, or for classes or any list that simply
+ * want to display a simple text view with a colored background
+ * @author Josh Ault
+ *
+ */
+public class SimpleListAdapter extends BaseAdapter {
+	
+	List<Object> items;
+	Context context;
+	
+	public SimpleListAdapter(Context context, List<Object> items) {
+		this.items = items;
+		this.context = context;
+	}
+
+	@Override
+	public int getCount() {
+		return items.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return items.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+    	LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    	View view = vi.inflate(R.layout.adapter_item, null);
+    	
+    	TextView txtGroup = (TextView) view.findViewById(R.id.adapter_item_lbl_name);
+    	
+    	txtGroup.setText(items.get(position).toString());
+    	
+		return view;
+	}
+
+}
